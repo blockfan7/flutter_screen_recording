29b6d19b0544be1eb55971cacef9a9649d0cdc5f
blockfan@outlook.com <blockfan@outlook.com>
2020-09-04 03:30:38 +0800

    add waitTime(Android)

--- android/src/main/kotlin/com/isvisoft/flutter_screen_recording/FlutterScreenRecordingPlugin.kt
+++ android/src/main/kotlin/com/isvisoft/flutter_screen_recording/FlutterScreenRecordingPlugin.kt
@@ -39,6 +39,7 @@ class FlutterScreenRecordingPlugin(
     var mDisplayHeight: Int = 800
     var storePath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).absolutePath + File.separator
     var videoName: String? = ""
+    var waitTime: Int ?= 0
     var recordAudio: Boolean? = false;
     private val SCREEN_RECORD_REQUEST_CODE = 333
     private val SCREEN_STOP_RECORD_REQUEST_CODE = 334
@@ -61,6 +62,7 @@ class FlutterScreenRecordingPlugin(
 
         if (requestCode == SCREEN_RECORD_REQUEST_CODE) {
             if (resultCode == Activity.RESULT_OK) {
+                Thread.sleep(waitTime*1000)
                 mMediaProjectionCallback = MediaProjectionCallback()
                 mMediaProjection = mProjectionManager?.getMediaProjection(resultCode, data)
                 mMediaProjection?.registerCallback(mMediaProjectionCallback, null)
@@ -90,6 +92,7 @@ class FlutterScreenRecordingPlugin(
 
                 videoName = call.argument("name")
                 recordAudio = call.argument("audio")
+                waitTime = call.argument("waitTime")
 
                 startRecordScreen()
                 //result.success(true)
--- lib/flutter_screen_recording.dart
+++ lib/flutter_screen_recording.dart
@@ -5,13 +5,13 @@ import 'package:flutter/services.dart';
 class FlutterScreenRecording {
   static const MethodChannel _channel = const MethodChannel('flutter_screen_recording');
 
-  static Future<bool> startRecordScreen(String name) async {
-    final bool start = await _channel.invokeMethod('startRecordScreen', {"name": name, "audio": false});
+  static Future<bool> startRecordScreen(String name, {int waitTime = 0}) async {
+    final bool start = await _channel.invokeMethod('startRecordScreen', {"name": name, "audio": false, "waitTime": waitTime});
     return start;
   }
 
-  static Future<bool> startRecordScreenAndAudio(String name) async {
-    final bool start = await _channel.invokeMethod('startRecordScreen', {"name": name, "audio": true});
+  static Future<bool> startRecordScreenAndAudio(String name, {int waitTime = 0}) async {
+    final bool start = await _channel.invokeMethod('startRecordScreen', {"name": name, "audio": true, "waitTime": waitTime});
     return start;
   }
 
